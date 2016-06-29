6493004dc229037bf431d7cf84a42d83e98124fc
Eric Davies <eric.davies@invenia.ca>
2016-06-29 16:26:27 -0500

    Tests using 3 processes.

--- test/runtests.jl
+++ test/runtests.jl
@@ -113,4 +113,46 @@ end
             end
         end
     end
+
+    @testset "3 process" begin
+        @testset "Example" begin
+            pnums = addprocs(2)
+            @everywhere using Dispatcher
+
+            try
+                ctx = DispatchContext()
+                exec = ParallelExecutor()
+
+                op = Op(()->3)
+                @test isempty(dependencies(op))
+                a = push!(ctx, op)
+
+                op = Op((x)->x, 4)
+                @test isempty(dependencies(op))
+                b = push!(ctx, op)
+
+                op = Op(max, a, b)
+                deps = dependencies(op)
+                @test a in deps
+                @test b in deps
+                c = push!(ctx, op)
+
+                op = Op(sqrt, c)
+                @test c in dependencies(op)
+                d = push!(ctx, op)
+
+                op = Op((x)->(rand(Int, x), rand(UInt, x)), c)
+                @test c in dependencies(op)
+                e, f = push!(ctx, op)
+
+                op = Op((x)->mean(x), f)
+                @test f in dependencies(op)
+                g = push!(ctx, op)
+
+                run(exec, ctx)
+            finally
+                rmprocs(pnums)
+            end
+        end
+    end
 end
